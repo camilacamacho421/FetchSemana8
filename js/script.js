40160f3b4d30f270ef88d9e7eaa4c4a54d93faef
camilacamacho421 <camilacamacho421@gmail.com>
2024-08-26 19:08:25 -0300

    Cambios en script

--- js/script.js
+++ js/script.js
@@ -14,8 +14,24 @@ function showData(dataArray) {
   // El for itera sobre los elementos del array
   for (const item of dataArray) {
     // En la siguiente línea se utilizan "backticks" para armar el String. Más info => https://developer.mozilla.org/es/docs/Web/JavaScript/Reference/Template_literals
-    container.innerHTML += `<p> ${item.name} ${item.lastname} </p>`; // Se concatena cada párrafo de la manera que queremos mostrarlo al innerHTML del contenedor
+    const datos = document.createElement('div');
+    datos.innerHTML += `<p> ${item.name} ${item.lastname} </p>`; // Se concatena cada párrafo de la manera que queremos mostrarlo al innerHTML del contenedor
+    container.appendChild(datos);
   }
 }
 
 // Escribe el código necesario para realizar el fetch al archivo con los datos y mostrar los estudiantes con la función showData
+fetch(DATA_URL)
+  .then(function (response) {
+    if (response.ok) {
+      return response.json();
+    } else {
+      console.log("Respuesta de red OK pero respuesta HTTP no OK");
+    }
+  })
+  .then(function(data){
+    showData(data.students);
+  })
+  .catch(function (error) {
+    console.log("Hubo un problema con la petición Fetch:" + error.message);
+  });
